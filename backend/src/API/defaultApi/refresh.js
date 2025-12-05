8565b67b9a95b3f79180a3f75b353d77adc69a2e
srihariprasanth123 <srihariprasanth123@gmail.com>
2025-12-05 12:30:45 +0000

    Updated resume section

--- backend/src/API/defaultApi/refresh.js
+++ backend/src/API/defaultApi/refresh.js
@@ -1,7 +1,7 @@
-exports.refreshRender = async (req,res) => {
-    try{
-        res.status(200).json({message:"Backend server started"})
-    }catch(err){
-        res.status(500).json({message:err.message})
+exports.refreshRender = async (req, res) => {
+    try {
+        res.status(200).json({ message: "Backend server started" })
+    } catch (err) {
+        res.status(500).json({ message: err.message })
     }
 }
--- backend/src/API/sendMail/sendmail_route.js
+++ backend/src/API/sendMail/sendmail_route.js
@@ -2,7 +2,7 @@ const { sendMail } = require("./sendmail_contoller")
 const express = require("express")
 const mailRouter = express.Router()
 
-mailRouter.post("/send-mail",sendMail)
+mailRouter.post("/send-mail", sendMail)
 
 module.exports = { mailRouter }
 
